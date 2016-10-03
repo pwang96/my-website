62ab2d2259bc4fe5c53fba3dc89036d1684bdc0e
pwang96 <96pwang@gmail.com>
2016-10-02 23:50:15 -0400

    testing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function openNav() {
 	document.getElementbyId("mysidenav").style.width = "250px";
-
+	document.getElementbyId("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
-	document.getElementbyId("mysidenav").style.width = "0";
+	document.getElementbyId("mysidenav").style.width = "5px";
+	document.getElementbyId("main").style.marginLeft = "5px";
 }
